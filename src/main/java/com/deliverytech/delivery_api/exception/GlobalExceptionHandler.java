3a1db5e2660d0603d17957f12eea1f3fd7bea35a
carlosDev <carloshenriquexvii@gmail.com>
2025-10-28 12:00:50 -0300

    refactor : evolução das exceptions.

--- src/main/java/com/deliverytech/delivery_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/deliverytech/delivery_api/exception/GlobalExceptionHandler.java
@@ -10,68 +10,108 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ApiErrorResponse> handleEntityNotFound(EntityNotFoundException ex){
-           ApiErrorResponse error = new ApiErrorResponse(
+    public ResponseEntity<ApiErrorResponse> handleEntityNotFound(EntityNotFoundException ex,  WebRequest request){
+        
+        Map<String, String> errors = new HashMap<>();
+
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
             HttpStatus.NOT_FOUND.value(),
             "Entidade não encontrada.",
             ex.getMessage(),
-            LocalDateTime.now()
+            LocalDateTime.now(),
+            request.getDescription(false).replace("uri=", ""),
+            "ENTITY_NOT_FOUND",
+            errors);
 
-        );
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
     
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ApiErrorResponse> handleBussinessException(BusinessException ex){
-        ApiErrorResponse error = new ApiErrorResponse(
+    public ResponseEntity<ApiErrorResponse> handleBussinessException(BusinessException ex, WebRequest request){
+        Map<String, String> errors = new HashMap<>();
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
             HttpStatus.BAD_REQUEST.value(),
             "Error de regra de negócio.",
             ex.getMessage(),
-            LocalDateTime.now()
+            LocalDateTime.now(),
+            request.getDescription(false).replace("uri=", ""),
+            "VALIDATION_ERROR",
+            errors 
 
         );
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationErrorResponse> handleValidationException(MethodArgumentNotValidException ex){
+    public ResponseEntity<ApiErrorResponse> handleValidationException(MethodArgumentNotValidException ex, WebRequest request){
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getAllErrors().forEach(error ->{
-            String fildName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fildName, errorMessage);
-        });
-
-        ValidationErrorResponse errorResponse = new ValidationErrorResponse(
-            HttpStatus.BAD_REQUEST.value(),
-            "Dados inválidos",
-            errors,
-            LocalDateTime.now()
-        );
+        ex.getBindingResult().getAllErrors().forEach((error) -> { 
+            String fieldName = ((FieldError) error).getField(); 
+            String errorMessage = error.getDefaultMessage(); 
+            errors.put(fieldName, errorMessage); 
+        }); 
+ 
+        ApiErrorResponse errorResponse = new ApiErrorResponse( 
+            HttpStatus.BAD_REQUEST.value(), 
+            "Dados inválidos", 
+            "Erro de validação nos dados enviados", 
+            LocalDateTime.now(),
+            request.getDescription(false).replace("uri=", ""),
+            "VALIDATION_ERROR",
+            errors 
+        ); 
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiErrorResponse> handleGenericExption(Exception ex){
-        ApiErrorResponse error = new ApiErrorResponse(
+    public ResponseEntity<ApiErrorResponse> handleGenericExption(Exception ex, WebRequest request){
+        Map<String, String> erros = new HashMap<>(); 
+        ApiErrorResponse errorResponse = new ApiErrorResponse(
             HttpStatus.INTERNAL_SERVER_ERROR.value(),
             "Erro interno do servidor",
             "Ocorreu um erro inesperado",
-            LocalDateTime.now()
+            LocalDateTime.now(),
+            request.getDescription(false).replace("uri=", ""),
+            "INTERNAL_ERROR",
+            erros 
         );
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 
+    }
+
+    @ExceptionHandler(ConflictException.class) 
+    public ResponseEntity<ApiErrorResponse> handleConflictException( 
+            ConflictException ex, WebRequest request) { 
+ 
+        Map<String, String> details = new HashMap<>(); 
+
+        if (ex.getConflictField() != null) { 
+            details.put(ex.getConflictField(), ex.getConflictValue().toString()); 
+        } 
+ 
+        ApiErrorResponse errorResponse = new ApiErrorResponse( 
+            HttpStatus.CONFLICT.value(), 
+            "Conflito de dados", 
+            "Erro de conflito de dados",
+            LocalDateTime.now(),
+            request.getDescription(false).replace("uri=", ""),
+            "CONFLICT_ERROR",
+            details 
+        ); 
+       
+ 
+       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
     
--- src/main/java/com/deliverytech/delivery_api/exception/ValidationErrorResponse.java
+++
@@ -1,7 +0,0 @@
-package com.deliverytech.delivery_api.exception;
-
-import java.time.LocalDateTime;
-import java.util.Map;
-
-public record ValidationErrorResponse(int status, String error, Map<String, String> errors, LocalDateTime timestamp) {
-} 
